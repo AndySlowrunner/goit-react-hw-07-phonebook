2cb518cfc5a8851ad6ce0fd37588619eadec2e15
Andy <andyfartushniy@gmail.com>
2023-11-20 12:56:02 +0200

    Updated file

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -14,7 +14,6 @@ export const ContactList = () => {
                     <p>{name}:{' ' + phone}</p>
                     <button onClick={() => {
                         dispatch(deleteContact(id));
-                        console.log(id);
                     }}>Delete</button>
                 </StyledList>
             )}
